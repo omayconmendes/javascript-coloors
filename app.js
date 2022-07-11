c985abfff3a7286992f06179c5d0e1ee756e1c57
Maycon Mendes <maycon.mendes@senior.com.br>
2022-07-10 22:16:35 -0300

    [FEAT] - Cria função para verificar contraste dos textos nos cards de cores.

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ function randomColors(){
         // Add the color to the background
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+
+        // Check for contrast
+        checkTextContrast(randomColor, hexText);
     });
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if(luminance > 0.5){
+        text.style.color = 'black';
+    } else {
+        text.style.color = 'white';
+    }
 }
 
 randomColors()
