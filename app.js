078a343af9cd9e8c0414ade913d08ef560339ff6
Maycon Mendes <maycon.mendes@senior.com.br>
2022-07-10 22:09:10 -0300

    [FEAT] - Cria função que gera cores com chroma js e adiciona aos cards.

---
+++ app.js
@@ -0,0 +1,26 @@
+// Global selectors and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+
+let initialColors;
+
+// Functions
+function generateHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors(){
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        // Add the color to the background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+
+randomColors()
